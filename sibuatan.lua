930b5d5026cf60bbbed2bc564edf5a9ade031b77
kenryuji71-blip <kenryuji71@gmail.com>
2025-08-22 02:19:25 +0700

    Update sibuatan.lua

--- sibuatan.lua
+++ sibuatan.lua
@@ -6,7 +6,7 @@ local char = player.Character or player.CharacterAdded:Wait()
 local locations = {
     ["Basecamp"]        = Vector3.new(987, 112, -698),
     ["Pos Pendakian"]   = Vector3.new(5200, 4000, 2100),
-    ["Puncak Sibuatan"] = Vector3.new(5322, 8119, 2150)
+    ["Puncak Sibuatan"] = Vector3.new(5344, 8113, 2117)
 }
 
 -- GUI utama
